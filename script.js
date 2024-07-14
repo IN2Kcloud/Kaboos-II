36bbb296f29237eb5971f5c9906877674b05b06b
SINXSIN <131200320+IN2Kcloud@users.noreply.github.com>
2024-07-14 16:30:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ window.addEventListener('load', () => {
 document.querySelector('.loadinger').addEventListener('transitionend', (e) => {
   document.body.removeChild(e.currentTarget);
 });
+
+// Select the .grid element as the container
+    const container = document.querySelector('.body');
+    const itemsWrapper = document.querySelector('.grid');
+
+    // Preload images
+    const preloadImages = () => {
+        return new Promise((resolve, reject) => {
+            imagesLoaded(document.querySelectorAll('img'), resolve);
+        });
+    };
+
+    // And then..
+    preloadImages().then(() => {
+        // Remove the loader
+        document.body.classList.remove('loading');
+        const effect = new RGBShiftEffect(container, itemsWrapper, { strength: 0.25 });
+    });    
